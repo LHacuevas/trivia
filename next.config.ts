b5c3a04dfddf6fd1cd3b8719957302b8756bcfe9
Alex Cuevasmi <cuevasmi@gmail.com>
2025-10-19 19:18:31 +0000

    et::ERR_BLOCKED_BY_CLIENT (anonymous) @ m=base:1936 zbb @ m=base:1938 wb

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
   basePath: '/trivia', // importante!
   typescript: {
     ignoreBuildErrors: true,
