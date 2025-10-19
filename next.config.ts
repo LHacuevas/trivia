d76041e3ddf6577f5d851599d24d034513de3029
Alex Cuevasmi <cuevasmi@gmail.com>
2025-10-19 19:25:11 +0000

    Algo falla me sale este en github como pagina principal trivia Firebase

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: '/trivia', // importante!
+  output: 'export',
+  basePath: '/trivia',
   typescript: {
     ignoreBuildErrors: true,
   },
